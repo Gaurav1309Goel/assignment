90ade502b6ca37edee0032aee255bdd19874b2d2
mananbansal2k <43756669+mananbansal2k@users.noreply.github.com>
2018-10-02 14:55:12 +0530

    Update Q4.py

--- Desktop/pythonassignmets/2 assignment/Q4.py
+++ Desktop/pythonassignmets/2 assignment/Q4.py
@@ -1,10 +1,10 @@
 l1 = [10,300,60,500]
 d1 = {}
-for x in l1:
+for y in l1:
         
-	f = (9.0/5.0)*x + 32
-	if not x in d1:
-                d1[x]=f
+	f = (9.0/5.0)*y + 32
+	if not y in d1:
+                d1[y]=f
 print d1
 
 	
