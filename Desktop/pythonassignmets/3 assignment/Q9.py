29517e737a3fd7f90e63159ae64fd2f4dbd65003
sohamk990 <43773509+sohamk990@users.noreply.github.com>
2018-10-02 15:54:48 +0530

    Update Q9.py

--- Desktop/pythonassignmets/3 assignment/Q9.py
+++ Desktop/pythonassignmets/3 assignment/Q9.py
@@ -1,11 +1,11 @@
 def deep_copy(l):
-    l1=[]
+    l2=[]
     for i in l:
         if type(i)=='list':
             return deep_copy(i)
         else:
-            l1.append(i)
-    return l1
+            l2.append(i)
+    return l2
 
-l=[1,2,3,[3,4,[5,66,7,8,8,[0,9,8,4,6]]]]
+l=[1,2,3,4,5,[3,4,[5,66,7,8,8,[0,9,8,4,6]]]]
 print deep_copy(l)
