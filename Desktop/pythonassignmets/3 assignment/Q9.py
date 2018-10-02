b4a39a082f1171eaf0b7d4b19ce486ee692d4aa4
k-udupa2000 <43147419+k-udupa2000@users.noreply.github.com>
2018-10-02 15:13:47 +0530

    Update Q9.py

--- Desktop/pythonassignmets/3 assignment/Q9.py
+++ Desktop/pythonassignmets/3 assignment/Q9.py
@@ -9,3 +9,13 @@ def deep_copy(l):
 
 l=[1,2,3,[3,4,[5,66,7,8,8,[0,9,8,4,6]]]]
 print deep_copy(l)
+def depth(l):
+    if(not type(l) == list):
+        return 0;
+    else:
+        d = []
+        for e in range(len(l)):
+            d.append(1 + depth(l[e]))
+        return max(d)
+print "This program will now give give the depth of the list that we have taken"
+print depth(l)
